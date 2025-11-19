c3566c5c551894c81b0e89a1b4626b6281fcc2ab
DibyajyotiR <routdibyajyoti278@gmail.com>
2025-11-19 09:48:03 +0530

    Route Chaining

--- WebB-NodeJS/Express/Basic-Server/index.js
+++ WebB-NodeJS/Express/Basic-Server/index.js
@@ -1,8 +1,39 @@
+//import express
 const express = require('express');
+
+//initialize express
 const app=express()
 
+//define route
+//we have to pass two arguments,1st is path,2nd is handler (maybe it is arrow func,callback func,anonymous func,normal func)      
+//bydefault home page route
 app.get('/',(req,res)=>{
     res.send("Hello,this is my express server")
+})
+//about route
+app.get('/about',(req,res)=>{
+    res.send("About us page")
+})
+//if we want to fetch data from client side /server side we will use GET method
+//for creating data will use POST method
+//for editing PUT method 
+//METHODS
+//GET -get data
+//POST -send data
+//PUT/PATCH -update data
+//DELETE -delete
+
+//contact route
+app.get('/contact',(req,res)=>{
+    res.send('contact us page')
+})
+//signup route
+app.post('/register',(req,res)=>{
+    res.send("Register route ")
+})
+
+app.get('/register',(req,res)=>{
+    res.send("Register route ")
 })
 app.listen(5000,()=>{
     console.log(`Server running on http://localhost:${5000}`);
---
+++ WebB-NodeJS/Express/mini-store/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app=express()
+
+//home
+app.get('/',(req,res)=>{
+    res.send("welcome to our store")
+})
+
+//product
+app.get('/product',(req,res)=>{
+    res.send("this is product page")
+})
+
+//about us
+app.get('/about',(req,res)=>{
+    res.send("this is about us page")
+})
+
+//contact us
+app.get('/contact',(req,res)=>{
+    res.send("this is contact us page")
+})
+//categories
+app.get('/categories',(req,res)=>{
+    res.send("this is categories page")
+})
+
+//Route parameters
+app.get("/product/:id",(req,res)=>{
+    console.log(req.params.id);
+    res.send(`you requested product with ID = ${req.params.id}`)
+    
+})
+
+app.listen(8000,()=>{
+    console.log(`Server running on http://localhost:${8000}`);
+    
+})
