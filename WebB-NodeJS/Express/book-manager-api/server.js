99376a1c649a71b2983de8f1506d9989557abc0d
DibyajyotiR <routdibyajyoti278@gmail.com>
2025-11-22 10:14:59 +0530

    manage-api

--- WebB-NodeJS/Express/book-manager-api/server.js
+++ WebB-NodeJS/Express/book-manager-api/server.js
@@ -21,6 +21,47 @@ app.post("/books",(req,res)=>{
     res.json({message:"Book Added",newBook,allBooks:books})
 })
 
+//!Get a book by ID
+app.get("/books/:id",(req,res)=>{
+    //get the id from params
+    //Number is a inbuilt method which convert string to number
+    const bookId=Number(req.params.id) //convert string to number
+    const bookFound=books.find((book)=>{
+        return book.id ===bookId
+    })
+    if(!bookFound){
+        return res.json({message:"Book Not Found"})
+    }
+    res.json(bookFound)
+})
+
+//!Delete a book by ID
+app.delete("/books/:id",(req,res)=>{
+    const bookId=Number(req.params.id)
+    books=books.filter((book)=>{
+        return book.id !== bookId
+    })
+    res.json({message:"Book deleted successfully",books})
+})
+
+
+//!Update book by ID
+app.put("/books/:id",(req,res)=>{
+    const bookId=Number(req.params.id)
+    const index=books.findIndex((book)=>{
+        return book.id === bookId
+    })
+    if(index=== -1){
+        res.json({message:"Book not found"})
+    }
+    //Update
+    //if we add (...)it means previous data will be same 
+    const updatedBook={...books[index],...req.body}
+    books[index]=updatedBook
+    res.json({message:"Book updated",data:updatedBook})
+})
+
+//!Start the server
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
     
